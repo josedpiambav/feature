fb131d782ea3d6706bd5348716a95a0d428cc941
José David Piamba Varela <jose.piamba@gaiavation.com>
2025-05-14 15:39:27 -0400

    remove

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,4 @@ set -e
   --repo "${INPUT_REPO}" \
   --trunk_branch "${INPUT_TRUNK_BRANCH}" \
   --target_branch "${INPUT_TARGET_BRANCH}" \
-  --merge_strategy "${INPUT_MERGE_STRATEGY}" \
   --labels "${INPUT_LABELS}"
