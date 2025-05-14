d08cab3fe90f236be78942432885fdf0933eee2d
José David Piamba Varela <jose.piamba@gaiavation.com>
2025-05-14 09:32:49 -0400

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 /usr/local/bin/feature-branching \
-  --github_token "$INPUT_GITHUB-TOKEN" \
-  --owner "$INPUT_OWNER" \
-  --repo "$INPUT_REPO" \
-  --trunk_branch "$INPUT_TRUNK-BRANCH" \
-  --target_branch "$INPUT_TARGET-BRANCH" \
-  --merge_strategy "$INPUT_MERGE-STRATEGY" \
-  --labels "$INPUT_LABELS"
+  --github_token "${INPUT_GITHUB_TOKEN}" \
+  --owner "${INPUT_OWNER}" \
+  --repo "${INPUT_REPO}" \
+  --trunk_branch "${INPUT_TRUNK_BRANCH}" \
+  --target_branch "${INPUT_TARGET_BRANCH}" \
+  --merge_strategy "${INPUT_MERGE_STRATEGY}" \
+  --labels "${INPUT_LABELS}"
